8af6771f9b5dd41fa6b3dd6624e9299eb906824e
Ricardo Mundo Mendez <ricardo.mundo.mendez@intel.com>
2015-06-18 17:21:52 -0500

    initial project version

---
+++ git_c_first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+  printf("Git commands \n\n\n");
+  
+  printf("Configurar GIT \n\n\n");
+  
+  printf("1 -  git config --global user.name 'Ricardo Mundo Mendez' \n");
+  printf("2 -  git config --global user.email ricardo.mundo.mendez@intel.com\n");
+  printf("3 - (optional) git config --global core.editor joe  ( optional not set) \n");
+  printf("4- git config --list \n");
+  printf("\n\n Para obtener ayuda:  git help <verb> e.g.  git help config \n\n");
+  printf(" Inicializar un repositorio de GIT      git init (en el directorio donde estara el repositorio) \n\n");
+  
+}  
